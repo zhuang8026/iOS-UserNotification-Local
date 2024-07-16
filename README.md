d9fa89eab26b8011abe34082e0328873049b1349
William.chuang <zhuang8026@gmail.com>
2024-07-16 11:57:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# UserNotification - Local - Practice
+Hi, I'm Lacie, an iOS developer.
+
+This is a project about practicing scheduling a local user notification.
+
+If you like this project, you can press ★Star to follow it.
+
+And if you are good at Chinese, you can see the [article1](https://ithelp.ithome.com.tw/articles/10226698) and [article2](https://ithelp.ithome.com.tw/articles/10227933) I wrote about the process while doing the project.
+
+## Demo
+
+<img src="https://github.com/yuyuma17/UserNotificationLocal/blob/master/Demo/demo1.gif"> 
---
+++ userNotificationLocal/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  userNotificationLocal
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createNotification()
+    }
+
+    func createNotification() {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "打我啊笨蛋"
+        content.subtitle = "哈哈"
+        content.body = "還敢下來啊冰鳥"
+        content.badge = 999
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "alarmMessage"
+
+        let imageURL = Bundle.main.url(forResource: "bird", withExtension: "png")
+        let attachment = try! UNNotificationAttachment(identifier: "", url: imageURL!, options: nil)
+        
+        content.attachments = [attachment]
+        
+        //Add specific time trigger
+//        var date = DateComponents()
+//        date.year = 2019
+//        date.month = 9
+//        date.day = 17
+//        date.hour = 12
+//        date.minute = 12
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+
+}
+
